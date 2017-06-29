0dbedaf1a19ac8ef7af6846fef915a5697772e42
Jon Manock <jonmanock@gmail.com>
2017-06-29 16:29:06 -0400

    delayed text works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+  /*
+    ~ Hide text for a few seconds
+    ~ Show text for a few seconds
+  */
+  $('.one, .two, .three').hide();
+  showText().delay('100');
+  function showText(){
+    $('.one').delay('2000').fadeIn();
+    $('.one').delay('2000').fadeOut();
+    $('.two').delay('5000').fadeIn();
+    $('.two').delay('2000').fadeOut();
+    $('.three').delay('8000').fadeIn();
+  }
+});
