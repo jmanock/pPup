5c4f6b3a5219d271d76152ffe8f28b0fbd2af031
Jon Manock <jonmanock@gmail.com>
2017-07-02 11:26:02 -0400

    cleaning up code, working on images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,12 @@
 $(document).ready(function(){
-  /*
-    ~ Hide text for a few seconds
-    ~ Show text for a few seconds
-  */
   $('.one, .two, .three').hide();
-  showText().delay('100');
-  function showText(){
-    $('.one').delay('2000').fadeIn();
-    $('.one').delay('2000').fadeOut();
-    $('.two').delay('5000').fadeIn();
-    $('.two').delay('2000').fadeOut();
-    $('.three').delay('8000').fadeIn();
-  }
-});
+
+}, showText('100'));
+
+function showText(){
+  $('.one').delay('2000').fadeIn();
+  $('.one').delay('2000').fadeOut();
+  $('.two').delay('5000').fadeIn();
+  $('.two').delay('2000').fadeOut();
+  $('.three').delay('8000').fadeIn();
+}// End `ShowText`
