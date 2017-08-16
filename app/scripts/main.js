7fe08623b884b0df4dd335c4ed395bef12c41572
Jon Manock <jonmanock@gmail.com>
2017-08-16 17:19:04 -0400

    not sure why it cuts off at the end

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,23 @@
+// $(document).ready(function(){
+//   $('.one, .two, .three').hide();
+//
+//   $('button').on('click', function(){
+//     $('.navbar-collapse').toggle();
+//   });
+//
+// }, showText('100'));
+//
+// function showText(){
+//   $('.one').delay('2000').fadeIn();
+//   $('.one').delay('2000').fadeOut();
+//   $('.two').delay('5000').fadeIn();
+//   $('.two').delay('2000').fadeOut();
+//   $('.three').delay('8000').fadeIn();
+// }// End `ShowText`
+
 $(document).ready(function(){
-  $('.one, .two, .three').hide();
-
-  $('button').on('click', function(){
-    $('.navbar-collapse').toggle();
-  });
-
+  $('.after').hide();
 }, showText('100'));
-
 function showText(){
-  $('.one').delay('2000').fadeIn();
-  $('.one').delay('2000').fadeOut();
-  $('.two').delay('5000').fadeIn();
-  $('.two').delay('2000').fadeOut();
-  $('.three').delay('8000').fadeIn();
-}// End `ShowText`
+  $('.after').delay('1000').fadeIn();
+}
