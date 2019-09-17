83e7e45c353370b27db49159486348966424c057
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-09-17 12:17:57 -0400

    update event

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+
+var $form = $('form#test-form');
+var url = 'https://script.google.com/macros/s/AKfycbwhLzhhNo4gWXnn-sWDFoVFq-ygEAUXQ2m0WpDB1_TT3O90IU8/exec';
+$('#submit-form').on('click', function(e){
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url:url,
+    method:'GET',
+    dataType:'json',
+    data:$form.serializeObject()
+  }).success(
+    console.log(data);
+  );
+})
